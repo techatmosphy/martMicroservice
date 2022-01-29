72a7f2954f57bea577d9a491f77ae3adb09d941b
techatmosphy <96334917+techatmosphy@users.noreply.github.com>
2022-01-30 01:41:04 +0530

    Update UserController.java

--- martMicroservice/src/main/java/com/jdr/martMicroservice/controller/UserController.java
+++ martMicroservice/src/main/java/com/jdr/martMicroservice/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.jdr.martMicroservice.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
