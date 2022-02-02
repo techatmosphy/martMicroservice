42d8bde1986c5cb30fc6d0003a8ea7adbf317e56
techatmosphy <info@techatmosphy.com>
2022-02-02 21:42:41 +0530

    Update Category
    Update Category

--- martMicroservice/src/main/java/com/jdr/martMicroservice/controller/CategoryController.java
+++ martMicroservice/src/main/java/com/jdr/martMicroservice/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package com.jdr.martMicroservice.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jdr.martMicroservice.entity.Category;
 import com.jdr.martMicroservice.service.CategoryService;
-
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoryController {
 
 	@Autowired
@@ -23,29 +27,29 @@ public class CategoryController {
 	@GetMapping("/category/{id}")
 	public ResponseEntity<Object> getCategory(@PathVariable Long id) {
 
-		categoryService.getCategory(id);
-		return new ResponseEntity<>(HttpStatus.OK);
+		Optional<Category> category=categoryService.getCategory(id);
+		return new ResponseEntity<>(category,HttpStatus.OK);
 	}
 
 	@GetMapping("/category")
 	public ResponseEntity<Object> getAllCategory() {
 
-		categoryService.getAllCategory();
-		return new ResponseEntity<>(HttpStatus.OK);
+		List<Category> category=categoryService.getAllCategory();
+		return new ResponseEntity<>(category,HttpStatus.OK);
 	}
 
 	@PostMapping("/category")
 	public ResponseEntity<Object> addCategory(@RequestBody Category category) {
 
-		categoryService.addCategory(category);
-		return new ResponseEntity<>(HttpStatus.OK);
+		Category RespCategory=categoryService.addCategory(category);
+		return new ResponseEntity<>(RespCategory,HttpStatus.OK);
 	}
 
 	@PutMapping
 	public ResponseEntity<Object> updateCategory(@RequestBody Category category) {
 
-		categoryService.updateCategory(category);
-		return new ResponseEntity<>(HttpStatus.OK);
+		Category RespCategory=categoryService.updateCategory(category);
+		return new ResponseEntity<>(RespCategory,HttpStatus.OK);
 	}
 
 	@DeleteMapping("/category/{id}")
